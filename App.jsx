cab61a3529e8b21c0c68158ea383135303c02e26
TonansisCeja <142059457+TonansisCeja@users.noreply.github.com>
2024-01-21 21:43:50 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+import {puppyList} from './data.js'
+import './App.css'
+import './index.css'
+
+function App() {
+ 
+const [featPupId, setFeatPupId] = useState(null)
+const [puppies, setPuppies] =useState(puppyList)
+console.log("puppyList: ", puppyList);
+
+function handleClick() {
+ 
+  // some logic here
+}
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+console.log(featuredPup)
+  return (
+    <>
+      <div className="App">
+        {puppies.map((puppy) => {
+           
+            return (
+              
+            <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+            </p>
+            
+            );
+          })}
+       {featPupId && (
+        <div className="pp">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li >Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}  
+      </div>
+   
+    
+    </>
+   );
+}
+
+export default App
+
+
